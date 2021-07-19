6de618b13deb3108abc61f510b65e989e7ab8d60
koduri-bit <mkod100@gmail.com>
2021-07-19 15:59:16 +0530

    updated code

--- src/components/JobList/Joblist.js
+++ src/components/JobList/Joblist.js
@@ -2,54 +2,22 @@ import {useContext ,Fragment} from 'react'
 import classes from './Joblist.module.css'
 import SelectedTags from '../JobList/SelectedTags'
 import JobListContext from "../../store/joblist-context";
-import {useMediaQuery} from "react-responsive";
+
 
 const Joblist = (props) =>{
 
     const ctx = useContext(JobListContext);
-    const isMobile = useMediaQuery({ query: `(max-width: 376px)` });
 
-    if(isMobile){
-        if(ctx.jobTagsDisplayState.length > 0){
-            const styles = classes['expand-margin-for-joblist'] + ' ' + classes['ul-joblist']
 
-            return (
-                <Fragment>
-                    <div className={classes['job-list']}>
+    const styleForJobListInMobile =  ctx.jobTagsDisplayState.length > 0 ? classes['ul-joblist'] + ' ' + classes['joblistExtraMargin'] : classes['ul-joblist'] ;
 
-                        {ctx.jobTagsDisplayState.length > 0 ? <SelectedTags  /> : null     }
-
-                        <ul className={styles}>
-                            {ctx.jobItemsInList}
-                        </ul>
-
-                    </div>
-                </Fragment>
-
-            )
-        }
-        return (
-            <Fragment>
-                <div className={classes['job-list']}>
-
-                    {ctx.jobTagsDisplayState.length > 0 ? <SelectedTags  /> : null     }
-
-                    <ul className={classes['ul-joblist']}>
-                        {ctx.jobItemsInList}
-                    </ul>
-
-                </div>
-            </Fragment>
-
-        )
-    }
 
     return (
         <Fragment>
 
             <div className={classes['job-list']}>
                 {ctx.jobTagsDisplayState.length > 0 ? <SelectedTags /> : null  }
-                <ul className={classes['ul-joblist']}>
+                <ul className={styleForJobListInMobile}>
                     {ctx.jobItemsInList}
                 </ul>
 
